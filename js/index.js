ac56fb6e89348dd2e7359e17075596f6de285010
Kah Yap <kahyap13@gmail.com>
2018-11-16 03:56:17 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -7,6 +7,13 @@ element.style.backgroundColor = '#f9f9f9';
 document.body.appendChild(element);
 
 element.style.textAlign = 'center';
-ul.style.textAlign = 'left';
 
-document.body.appendChild(element);
+var ul = document.createElement('ul');
+
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+
+element.appendChild(ul);
